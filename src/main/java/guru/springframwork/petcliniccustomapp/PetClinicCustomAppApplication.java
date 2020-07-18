ebce27a3ec1f8200ad2638a942b14cb71002e972
svnsairam <svnsairam.sakhamuri@gmail.com>
2020-07-18 15:03:13 +0530

    Initial Commit

---
+++ src/main/java/guru/springframwork/petcliniccustomapp/PetClinicCustomAppApplication.java
@@ -0,0 +1,13 @@
+package guru.springframwork.petcliniccustomapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicCustomAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetClinicCustomAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/guru/springframwork/petcliniccustomapp/PetClinicCustomAppApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframwork.petcliniccustomapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicCustomAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
